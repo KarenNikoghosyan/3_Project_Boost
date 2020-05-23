99f9a9d5a25487619588cc01f417698ae2cb279c
Karenmax11 <karen.nikoghosyan11@gmail.com>
2020-05-23 17:19:31 +0300

    15_PB_CU2 Basic Level Design

--- 3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -35,6 +35,7 @@ public class Rocket : MonoBehaviour
                 print("Refueling");
                 break;
             default:
+                Destroy(gameObject);
                 print("Dead");
                 break;
         }
